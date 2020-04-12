f3a1f2b9f1d2f597837a8e39997ee4b82d19657b
Rizqy H <rizqy22@gmail.com>
2020-04-12 10:26:38 +0700

    Using functions redirection for laporan, since route redirect is deprecated on Now 2.0

---
+++ api/laporan.js
@@ -0,0 +1,6 @@
+module.exports = (req, res) => {
+  res.writeHead(302,
+    {Location: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSjdVXwOE8l6z3PFkwt7IXFU9uPZaVurzT5L-jBzEMYL79d6I1-35Iym437LCfALqeJRjDZrYJ0bZpn/pubhtml?gid=290734302&single=true'}
+  );
+  res.end();
+}
